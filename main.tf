432c0b446f2c029d6d9bbc280080fed38cce4ecc
ablaeklou <odestaremawusi@gmail.com>
2023-10-07 22:25:21 -0400

    modifing few things so i can create the user on AWS

--- main.tf
+++ main.tf
@@ -1,57 +1,39 @@
 ##https://registry.terraform.io/
-  
+
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.61.0"
     }
-    
+
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  
 }
 
-
-##*** main.tf
-
-terraform {
-
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "4.61.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-
-}
 
 resource "aws_iam_group" "demo" {
-    name = "security"
+  name = "security"
 
 }
 resource "aws_iam_user" "user1" {
-    name = "serge2023"
-  
+  name = "ode2023"
+
 }
 
 
 ##**** user.tf 
 
 resource "aws_iam_user" "user-demo" {
-  name = "serge2026"
+  name = "ode2026"
   tags = {
     Team                = "Devops"
     Env                 = "Dev"
     create_by_terraform = "yes"
-    Owner               = "serge"
+    Owner               = "ode"
     email               = "dimitri@gmail.com"
 
   }
@@ -59,7 +41,7 @@ resource "aws_iam_user" "user-demo" {
 
 ##**** group.tf
 
-resource "aws_iam_group" "demo" {
+resource "aws_iam_group" "demo1" {
   name = "terraform-group"
 }
 
@@ -72,12 +54,12 @@ output "group-arn" {
   value = aws_iam_group.demo.arn
 }
 output "group-id" {
-    value = aws_iam_group.demo.id 
+  value = aws_iam_group.demo.id
 }
 output "username" {
-    value = aws_iam_user.user-demo.name  
+  value = aws_iam_user.user-demo.name
 }
 output "user-tags" {
-    value = aws_iam_user.user-demo.tags_all
-  
+  value = aws_iam_user.user-demo.tags_all
+
 }
